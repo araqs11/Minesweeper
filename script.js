6410b1c268b9ea815b818a23fc3685dbb897222e
Araqs <51331697+araqs11@users.noreply.github.com>
2021-11-27 06:23:28 +0100

    finished generating the board
    completed the board generation for the mines & numbers

--- script.js
+++ script.js
@@ -6,27 +6,209 @@ easy    8  x  8 10 mines 64 tiles
 medium  16 x 16 40 mines 256 tiles
 hard    22 x 22 99 mines 484 tiles
 */
+var field;
+
 function startGame() {
+    generateField();
+    generateMines();
+    generateNumbers();
     generateGrid();
-    //generateMines();
-    //generateNumbers();
+
     
 }
 
-function generateGrid() {
+function create2DArray(size) {
+    var outerArray = [];
+    for (let i = 0; i < size; i++) {
+        var innerArray = [];
+        for (let j = 0; j < size; j++) {
+            innerArray[j] = 0;
+        }
+        outerArray[i] = innerArray;
+    }
+    return outerArray;
+}
+
+function generateField() {
     let gameGrid = document.getElementById('gameGrid');
     var input = document.getElementById('difficulty').selectedIndex;
-    var field;
-
+    
     gameGrid.innerHTML = "";
 
     switch(input) {
-        case 0: field = createField(8); break;
-        case 1: field = createField(16); break;
-        case 2: field = createField(22); break;
+        case 0: field = create2DArray(8); break;
+        case 1: field = create2DArray(16); break;
+        case 2: field = create2DArray(22); break;
         default: console.log("ERROR AT CREATING FIELD");
     }
+}
 
+
+function generateMines() {
+    var mines = 0;
+    switch (field.length) {
+        case 8: mines = 10;break;
+        case 16: mines = 40;break;
+        case 22: mines = 99;break;
+        default: console.log("ERROR AT GENERATING MINES");
+    }
+
+    for (let i = 0; i < mines; i++) {
+        var minePosX = Math.floor(Math.random()*field.length);
+        var minePosY = Math.floor(Math.random()*field.length);
+
+        if(field[minePosX][minePosY] == 9) {
+            i--;
+        } else {
+            field[minePosX][minePosY] = 9;
+        }
+    }
+}
+
+function generateNumbers() {
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field.length; j++) {
+            var counter = 0;
+            if(field[i][j] == 0) {
+
+                if(j==0 && i==0) {
+                    if(field[i][j+1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j+1] == 9) {
+                        counter++; 
+                    }
+                } else if(j==field.length-1 && i==0) {
+                    if(field[i][j-1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j-1] == 9) {
+                        counter++; 
+                    }
+                } else if(j==0 && i==field.length-1) {
+                    if(field[i][j+1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j+1] == 9) {
+                        counter++; 
+                    }
+                } else if(j==field.length-1 && i==field.length-1) {
+                    if(field[i][j-1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j-1] == 9) {
+                        counter++; 
+                    }
+                } else if(j==0) {
+                    if(field[i][j+1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j+1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j+1] == 9) {
+                        counter++; 
+                    }
+                } else if(j==field.length-1) {
+                    if(field[i][j-1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j-1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j-1] == 9) {
+                        counter++; 
+                    }
+                } else if(i==0) {
+                    if(field[i][j+1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i][j-1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j+1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j-1] == 9) {
+                        counter++; 
+                    }
+                } else if(i==field.length-1) {
+                    if(field[i][j+1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i][j-1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j-1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j-1] == 9) {
+                        counter++; 
+                    }
+                }
+                else {
+                    if(field[i][j+1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i][j-1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j+1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i+1][j-1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j+1] == 9) {
+                        counter++; 
+                    }
+                    if(field[i-1][j-1] == 9) {
+                        counter++; 
+                    }
+                }
+                field[i][j] = counter;
+            }
+        }        
+    }
+}
+
+function generateGrid() {
     for (let i = 0; i < field.length; i++) {
         for (let j = -1; j < field.length; j++) {
             var new_element;
@@ -34,21 +216,11 @@ function generateGrid() {
                 new_element = document.createElement('tr');
             } else {
                 new_element = document.createElement('td');
-                //new_element.textContent = field[i][j];
+                if(field[i][j]) {
+                } 
+                new_element.textContent = field[i][j];
             }
             gameGrid.appendChild(new_element);
         }
     }
-}
-
-function createField(size) {
-    var field = [];
-    for (let i = 0; i < size; i++) {
-        var tmp = [];
-        for (let j = 0; j < size; j++) {
-            tmp[j] = 0;
-        }
-        field[i] = tmp;
-    }
-    return field;
 }
