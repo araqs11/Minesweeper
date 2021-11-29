e62c4ab9bdd3bec3640ae238d982443bef29833c
Araqs <51331697+araqs11@users.noreply.github.com>
2021-11-29 03:26:25 +0100

    changed the way data is stored & added click func
    field has now one value and one tile data per index & added click function for the td's - added gameOver screen & prepared the check function if player hit a blank tile

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function create2DArray(size) {
     for (let i = 0; i < size; i++) {
         var innerArray = [];
         for (let j = 0; j < size; j++) {
-            innerArray[j] = 0;
+            innerArray[j] = {value:0,tile:undefined};
         }
         outerArray[i] = innerArray;
     }
@@ -57,10 +57,10 @@ function generateMines() {
         var minePosX = Math.floor(Math.random()*field.length);
         var minePosY = Math.floor(Math.random()*field.length);
 
-        if(field[minePosX][minePosY] == 9) {
+        if(field[minePosX][minePosY].value == 9) {
             i--;
         } else {
-            field[minePosX][minePosY] = 9;
+            field[minePosX][minePosY].value = 9;
         }
     }
 }
@@ -69,140 +69,140 @@ function generateNumbers() {
     for (let i = 0; i < field.length; i++) {
         for (let j = 0; j < field.length; j++) {
             var counter = 0;
-            if(field[i][j] == 0) {
+            if(field[i][j].value == 0) {
 
                 if(j==0 && i==0) {
-                    if(field[i][j+1] == 9) {
+                    if(field[i][j+1].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j] == 9) {
+                    if(field[i+1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j+1] == 9) {
+                    if(field[i+1][j+1].value == 9) {
                         counter++; 
                     }
                 } else if(j==field.length-1 && i==0) {
-                    if(field[i][j-1] == 9) {
+                    if(field[i][j-1].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j] == 9) {
+                    if(field[i+1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j-1] == 9) {
+                    if(field[i+1][j-1].value == 9) {
                         counter++; 
                     }
                 } else if(j==0 && i==field.length-1) {
-                    if(field[i][j+1] == 9) {
+                    if(field[i][j+1].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j] == 9) {
+                    if(field[i-1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j+1] == 9) {
+                    if(field[i-1][j+1].value == 9) {
                         counter++; 
                     }
                 } else if(j==field.length-1 && i==field.length-1) {
-                    if(field[i][j-1] == 9) {
+                    if(field[i][j-1].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j] == 9) {
+                    if(field[i-1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j-1] == 9) {
+                    if(field[i-1][j-1].value == 9) {
                         counter++; 
                     }
                 } else if(j==0) {
-                    if(field[i][j+1] == 9) {
+                    if(field[i][j+1].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j] == 9) {
+                    if(field[i+1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j] == 9) {
+                    if(field[i-1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j+1] == 9) {
+                    if(field[i+1][j+1].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j+1] == 9) {
+                    if(field[i-1][j+1].value == 9) {
                         counter++; 
                     }
                 } else if(j==field.length-1) {
-                    if(field[i][j-1] == 9) {
+                    if(field[i][j-1].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j] == 9) {
+                    if(field[i+1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j] == 9) {
+                    if(field[i-1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j-1] == 9) {
+                    if(field[i+1][j-1].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j-1] == 9) {
+                    if(field[i-1][j-1].value == 9) {
                         counter++; 
                     }
                 } else if(i==0) {
-                    if(field[i][j+1] == 9) {
+                    if(field[i][j+1].value == 9) {
                         counter++; 
                     }
-                    if(field[i][j-1] == 9) {
+                    if(field[i][j-1].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j] == 9) {
+                    if(field[i+1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j+1] == 9) {
+                    if(field[i+1][j+1].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j-1] == 9) {
+                    if(field[i+1][j-1].value == 9) {
                         counter++; 
                     }
                 } else if(i==field.length-1) {
-                    if(field[i][j+1] == 9) {
+                    if(field[i][j+1].value == 9) {
                         counter++; 
                     }
-                    if(field[i][j-1] == 9) {
+                    if(field[i][j-1].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j] == 9) {
+                    if(field[i-1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j-1] == 9) {
+                    if(field[i-1][j-1].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j-1] == 9) {
+                    if(field[i-1][j+1].value == 9) {
                         counter++; 
                     }
                 }
                 else {
-                    if(field[i][j+1] == 9) {
+                    if(field[i][j+1].value == 9) {
                         counter++; 
                     }
-                    if(field[i][j-1] == 9) {
+                    if(field[i][j-1].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j] == 9) {
+                    if(field[i+1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j] == 9) {
+                    if(field[i-1][j].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j+1] == 9) {
+                    if(field[i+1][j+1].value == 9) {
                         counter++; 
                     }
-                    if(field[i+1][j-1] == 9) {
+                    if(field[i+1][j-1].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j+1] == 9) {
+                    if(field[i-1][j+1].value == 9) {
                         counter++; 
                     }
-                    if(field[i-1][j-1] == 9) {
+                    if(field[i-1][j-1].value == 9) {
                         counter++; 
                     }
                 }
-                field[i][j] = counter;
+                field[i][j].value = counter;
             }
         }        
     }
@@ -211,16 +211,49 @@ function generateNumbers() {
 function generateGrid() {
     for (let i = 0; i < field.length; i++) {
         for (let j = -1; j < field.length; j++) {
-            var new_element;
+            var tile;
             if(j==-1) {
-                new_element = document.createElement('tr');
+                tile = document.createElement('tr');
             } else {
-                new_element = document.createElement('td');
-                if(field[i][j]) {
-                } 
-                new_element.textContent = field[i][j];
+                tile = document.createElement('td');
+                tile.value = field[i][j].value;
+                field[i][j].tile = tile;
+                //tile.textContent = field[i][j].value;
+                tile.onclick = function() {click(field[i][j])}
+                
             }
-            gameGrid.appendChild(new_element);
+            gameGrid.appendChild(tile);
         }
     }
 }
+
+function click(element) {
+    if(element.value == 9) {
+        gameOver();
+    }
+    if(element.value) {
+        element.tile.textContent = element.value;
+        element.tile.style.backgroundColor = "gray";
+    } else {
+        checkIfNextIsClean(element);
+    }
+    
+}
+
+function gameOver() {
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field.length; j++) {
+            if(field[i][j].value == 9) {
+                field[i][j].tile.style.backgroundColor = "red";
+            } else {
+                field[i][j].tile.style.backgroundColor = "orange";
+            }   
+            field[i][j].tile.textContent = field[i][j].value;
+            field[i][j].tile.onclick = undefined;
+        }
+    }
+}
+ function checkIfNextIsClean(element) {
+     
+ }
+
